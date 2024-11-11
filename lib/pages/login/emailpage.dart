5523151e91c2536b754a07a09d32312ea0b2b294
Daniel B <Dragon863.dev@gmail.com>
2024-11-11 17:49:23 +0000

    Email page changes

--- lib/pages/login/emailpage.dart
+++ lib/pages/login/emailpage.dart
@@ -1,5 +1,6 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:runshaw/main.dart';
 import 'package:runshaw/pages/main/main_view.dart';
@@ -18,17 +19,15 @@ class _EmailPageState extends State<EmailPage> {
   void initState() {
     super.initState();
     final BaseAPI api = context.read<BaseAPI>();
-    api.addListener(
-      () {
-        if (api.status == AccountStatus.authenticated) {
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (context) => const BaseApp(),
-            ),
-          );
-        }
-      },
-    );
+    api.addListener(() {
+      if (api.status == AccountStatus.authenticated) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const BaseApp(),
+          ),
+        );
+      }
+    });
   }
 
   @override
@@ -39,73 +38,92 @@ class _EmailPageState extends State<EmailPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.emailAddress,
-              controller: emailController,
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(
+              minWidth: 150,
+              maxWidth: 500,
             ),
-            const SizedBox(height: 12),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.emailAddress,
-              controller: passwordController,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final BaseAPI api = context.read<BaseAPI>();
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Nice to meet you!",
+                  style: GoogleFonts.rubik(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const Text("Let's get you logged in..."),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                ),
+                const SizedBox(height: 12),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  controller: passwordController,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    final BaseAPI api = context.read<BaseAPI>();
 
-                try {
-                  await api.createUser(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-                } on AppwriteException catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(e.message ?? 'An error occurred'),
-                    ),
-                  );
-                }
-              },
-              child: const Text('Sign Up'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                final BaseAPI api = context.read<BaseAPI>();
+                    try {
+                      await api.createUser(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
+                    } on AppwriteException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(e.message ?? 'An error occurred'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Sign Up'),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    final BaseAPI api = context.read<BaseAPI>();
 
-                try {
-                  await api.createEmailSession(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const MainPage(),
-                    ),
-                  );
-                } on AppwriteException catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(e.message ?? 'An error occurred'),
-                    ),
-                  );
-                }
-              },
-              child: const Text('Sign In'),
+                    try {
+                      await api.createEmailSession(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MainPage(),
+                        ),
+                      );
+                    } on AppwriteException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(e.message ?? 'An error occurred'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Sign In'),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
