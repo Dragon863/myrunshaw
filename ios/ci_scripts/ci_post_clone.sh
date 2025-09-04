76dd3bd1391a0a55aa0081fbbf4d38e26db8d716
Daniel B <Dragon863.dev@gmail.com>
2025-09-04 01:03:28 +0100

    Update Kotlin plugin version and adjust CocoaPods installation script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -28,12 +28,11 @@ echo "Installing cocoapods..."
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 
-# handle OneSignalXCFramework
-pod update OneSignalXCFramework
-
 # Set up CocoaPods for iOS
 echo "Running pod install for iOS..."
 cd ios
+# handle OneSignalXCFramework
+pod update OneSignalXCFramework
 pod install --repo-update
 
 # Go back to the workspace root
